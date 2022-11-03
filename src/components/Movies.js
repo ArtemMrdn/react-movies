743d74c5efa981d1816e885804693da068320068
ArtomMrdn <artommrdn47@gmail.com>
2022-11-03 10:23:12 +0200

    init commit

---
+++ src/components/Movies.js
@@ -0,0 +1,18 @@
+import Movie from "./Movie";
+
+function Movies(props) {
+  const { movies = [] } = props;
+  return (
+    <div className='movies'>
+      {movies.length ? (
+        movies.map((movie) => {
+          return <Movie key={movie.imdbID} {...movie} />;
+        })
+      ) : (
+        <h3>Nothing found</h3>
+      )}
+    </div>
+  );
+}
+
+export default Movies;
